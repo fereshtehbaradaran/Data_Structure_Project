3620dd9495981aa5dbce7ce969d082b94d0ae99c
fereshtehbaradaran <bara.frsht@gmail.com>
2023-01-11 12:13:16 +0330

    added node.py

---
+++ node.py
@@ -0,0 +1,8 @@
+class node(object):
+    def __init__(self, first, second, third):
+        self.first = first
+        self.second = second
+        self.third = third
+        self.left = None
+        self.right = None
+        self.height = 1
