8923f1eb5cb76775ff1d19a721deea4bc637560f
fereshtehbaradaran <bara.frsht@gmail.com>
2023-01-11 12:35:19 +0330

    added main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import AVL_Tree
+
+patientsBalancedByID = AVL_Tree()
+patientsBalancedByIDRoot = None
+patientsBalancedByHM = AVL_Tree()
+patientsBalancedByHMRoot = None
+patientsBalancedByOrder = AVL_Tree()
+patientsBalancedByOrderRoot = None
+numberOfPatients = 0
+
