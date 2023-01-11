2e980888e83cff611fd33ffa803049fe7a5a1062
fereshtehbaradaran <bara.frsht@gmail.com>
2023-01-11 12:23:44 +0330

    delete added to AVL tree

--- AVL_Tree.py
+++ AVL_Tree.py
@@ -30,3 +30,56 @@ class AVL_Tree(object):
             return self.leftRotate(root)
  
         return root
+
+    
+    def delete(self, root, nodeProperty):
+
+        if not root:
+            return root
+
+        elif nodeProperty < root.first:
+            root.left = self.delete(root.left, nodeProperty)
+
+        elif nodeProperty > root.first:
+            root.right = self.delete(root.right, nodeProperty)
+
+        else:
+            if root.left is None:
+                temp = root.right
+                root = None
+                return temp
+
+            elif root.right is None:
+                temp = root.left
+                root = None
+                return temp
+
+            temp = self.getMinValueNode(root.right)
+            root.first = temp.first
+            root.second = temp.second
+            root.third = temp.third
+            root.right = self.delete(root.right, temp.first)
+
+        if root is None:
+            return root
+
+        root.height = 1 + max(self.getHeight(root.left),
+                            self.getHeight(root.right))
+
+        balance = self.getBalance(root)
+
+        if balance > 1 and self.getBalance(root.left) >= 0:
+            return self.rightRotate(root)
+
+        if balance < -1 and self.getBalance(root.right) <= 0:
+            return self.leftRotate(root)
+
+        if balance > 1 and self.getBalance(root.left) < 0:
+            root.left = self.leftRotate(root.left)
+            return self.rightRotate(root)
+
+        if balance < -1 and self.getBalance(root.right) > 0:
+            root.right = self.rightRotate(root.right)
+            return self.leftRotate(root)
+
+        return root
