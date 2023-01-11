cf6a56b05e39081c778ce0286ae801b542598fe0
fereshtehbaradaran <bara.frsht@gmail.com>
2023-01-11 12:36:14 +0330

    add patient added

--- main.py
+++ main.py
@@ -8,3 +8,15 @@ patientsBalancedByOrder = AVL_Tree()
 patientsBalancedByOrderRoot = None
 numberOfPatients = 0
 
+
+
+def add(ID,HM):
+    global patientsBalancedByIDRoot
+    global patientsBalancedByHMRoot
+    global patientsBalancedByOrderRoot
+    global numberOfPatients
+    
+    patientsBalancedByIDRoot = patientsBalancedByID.insert(patientsBalancedByIDRoot,node(ID,HM,numberOfPatients))
+    patientsBalancedByHMRoot = patientsBalancedByHM.insert(patientsBalancedByHMRoot,node(HM,ID,numberOfPatients))
+    patientsBalancedByOrderRoot = patientsBalancedByOrder.insert(patientsBalancedByOrderRoot,node(numberOfPatients, ID, HM))
+    numberOfPatients += 1
